b12076acaea3391260440b805e7cf8f1b1e43198
Anton Perevoshchikov <anper3.5@gmail.com>
2020-05-03 15:27:01 +0300

    Adds schema test

--- tests/SchemaTest.php
+++ tests/SchemaTest.php
@@ -3,10 +3,70 @@
 namespace Anper\Iuliia\Tests\Schema;
 
 use Anper\Iuliia\Iuliia;
+use Anper\Iuliia\Map;
+use Anper\Iuliia\Schema;
 use PHPUnit\Framework\TestCase;
 
 class SchemaTest extends TestCase
 {
+    public function testGetMaps(): void
+    {
+        $default = new Map(['d' => 'd']);
+        $prev = new Map(['p' => 'p']);
+        $next = new Map(['n' => 'n']);
+        $ending = new Map(['e' => 'e']);
+        $samples = ['s' => 's'];
+
+        $schema = new Schema($default, $prev, $next, $ending, $samples);
+
+        $this->assertEquals($default, $schema->getDefaultMap());
+        $this->assertEquals($prev, $schema->getPrevMap());
+        $this->assertEquals($next, $schema->getNextMap());
+        $this->assertEquals($ending, $schema->getEndingMap());
+        $this->assertEquals($samples, $schema->getSamples());
+    }
+
+    public function testCreateFromNotExistsFile(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        Schema::createFromFile('not.exists');
+    }
+
+    public function testCreateFromFileWhereFileIsDir(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        Schema::createFromFile(__DIR__);
+    }
+
+    public function testCreateFromFileWithEmptyContent(): void
+    {
+        $this->expectException(\RuntimeException::class);
+
+        Schema::createFromFile(__DIR__ . '/files/empty.php');
+    }
+
+    public function testCreateFromFileWhereContentIsNotArray(): void
+    {
+        $this->expectException(\RuntimeException::class);
+
+        Schema::createFromFile(__DIR__ . '/files/date.php');
+    }
+
+    public function testCreateFromFile(): void
+    {
+        $data = include __DIR__ . '/files/content.php';
+
+        $schema = Schema::createFromFile(__DIR__ . '/files/content.php');
+
+        $this->assertEquals($data[0], $schema->getDefaultMap()->all());
+        $this->assertEquals($data[1], $schema->getPrevMap()->all());
+        $this->assertEquals($data[2], $schema->getNextMap()->all());
+        $this->assertEquals($data[3], $schema->getEndingMap()->all());
+        $this->assertEquals($data[4], $schema->getSamples());
+    }
+
     public function testTranslate(): void
     {
         foreach (Iuliia::SCHEMAS as $schemaId) {
